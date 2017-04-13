e50798568a6775f8a4a8612c75fc85cd0435eff5
Jeffrey Li <jeffrey.li@cantab.net>
2017-04-13 19:30:29 +0800

    all the requirements completed and functional

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Shoe_Distribution
 
-#### As a user, I want to be able to add, update, delete and list shoe stores.
-#### As a user, I want to be able to add and list new shoe brands.
-#### As a user, I want to be able to add shoe brands in the application (don't worry about updating, listing or destroying shoe brands).
-#### As a user, I want to be able to add existing shoe brands to a store to show where they are sold.
-#### As a user, I want to be able to associate the same brand of shoes with multiple stores.
-#### As a user, I want to be able to see all of the brands a store sells on the individual store page.
-#### As a user, I want store names and shoe brands to be saved with a capital letter no matter how I enter them.
-#### As a user, I do not want stores and/or shoe brands to be saved if I enter a blank name
+#### As a user, I want to be able to add, update, delete and list shoe stores.(done)
+#### As a user, I want to be able to add and list new shoe brands.(done)
+#### As a user, I want to be able to add shoe brands in the application (don't worry about updating, listing or destroying shoe brands).(done)
+#### As a user, I want to be able to add existing shoe brands to a store to show where they are sold.(done)
+#### As a user, I want to be able to associate the same brand of shoes with multiple stores.(done)
+#### As a user, I want to be able to see all of the brands a store sells on the individual store page.(done)
+#### As a user, I want store names and shoe brands to be saved with a capital letter no matter how I enter them.(done)
+#### As a user, I do not want stores and/or shoe brands to be saved if I enter a blank name(done)
 
 ## Technologies Used
 
--- app.rb
+++ app.rb
@@ -11,10 +11,92 @@ get("/") do
   erb(:index)
 end
 
+delete('/database_reset') do
+  @brands=Brand.all()
+  @stores=Store.all()
+  @brands.each() do |brand|
+  brand.destroy()
+  end
+  @stores.each() do |store|
+    store.destroy()
+  end
+  redirect('/')
+end
+
 get('/add_brands') do
+  @brands=Brand.all()
   erb(:add_brand)
 end
 
 get('/add_stores') do
+  @stores=Store.all()
   erb(:add_store)
+end
+
+post('/add_stores') do
+  name = params.fetch("name")
+  Store.create({:name => name})
+  redirect back
+end
+
+post('/add_brands') do
+  name = params.fetch("name")
+  Brand.create({:name => name})
+  redirect back
+end
+
+delete('/stores/:id/delete') do
+  store = Store.find(params.fetch("id"))
+  store.destroy()
+  redirect back
+end
+
+get("/stores/:id/edit") do
+  @store = Store.find(params.fetch("id"))
+  erb(:store_edit)
+end
+
+patch("/stores/:id/edit") do
+  store = Store.find(params.fetch("id"))
+  name = params.fetch("name")
+  store.update({:name => name})
+  redirect ('/add_stores')
+end
+
+get('/view_brands') do
+  @brands = Brand.all()
+  erb(:view_brands)
+end
+
+get('/view_stores') do
+  @stores = Store.all()
+  erb(:view_stores)
+end
+
+get('/stores/:id') do
+  @store = Store.find(params.fetch("id"))
+  @brands = Brand.all
+  erb(:store)
+end
+
+get('/brands/:id') do
+  @brand = Brand.find(params.fetch("id"))
+  @stores = Store.all
+  erb(:brand)
+end
+
+patch("/stores/:id/add_brand") do
+  store = Store.find(params.fetch("id"))
+  brand_id = params.fetch("brand_id")
+  brand = Brand.find(brand_id)
+  store.brands.push(brand)
+  redirect back
+end
+
+patch("/brands/:id/add_store") do
+  brand = Brand.find(params.fetch("id"))
+  store_id = params.fetch("store_id")
+  store = Store.find(store_id)
+  brand.stores.push(store)
+  redirect back
 end
